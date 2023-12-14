f5e82e9781a1cf857c0d2d0bf323c593f87780a6
Rohit Katakam <rohitkatakam@gmail.com>
2023-12-14 15:51:58 +0530

    Initial Commit

---
+++ LiftAnalyzer/LiftAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftAnalyzerApp.swift
+//  LiftAnalyzer
+//
+//  Created by Rohit Katakam on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct LiftAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
